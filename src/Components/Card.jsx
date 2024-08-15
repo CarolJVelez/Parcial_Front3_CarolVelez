d9c26d342009431066ba4af35ffc074e4b54875a
carol velez <carol.velez@supergiros.com.co>
2024-08-14 20:15:25 -0500

    Se entrega el parcial front3

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
 
-const Card = () => {
+const Card = ({customer, setShow}) => {
+  const {name, age, car} = customer
+
+  const handleClick = (event) => {
+    setShow(false);
+  }
+
   return (
-    <div>Hola, soy una tarjeta</div>
+    <div>
+        <h4>Bienvenid@, {name}. Tu edad es: {age} </h4>
+        <h5>{car} Esa marca de carro es increible.!!</h5>
+        <button type="button" onClick={handleClick}>Volver</button>
+    </div>
   )
 }
 
